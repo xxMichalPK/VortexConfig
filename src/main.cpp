6edd64a937fbf61d8c6168fd10afffcb5a03c099
xxMichalPk <michal10.05.pk@gmail.com>
2024-06-18 11:01:30 +0200

    Basic test app and sample file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 ﻿// #define CFV_BUFFER_ONLY 1
 // #define CFV_EMBEDDED_FUNCTIONS 1
 #include "VortexConfig.h"
+#include <iostream>
 
 // Here we test the library
 int main() {
@@ -8,20 +9,34 @@ int main() {
 		printf("Failed to open file!\n\r");
 	}
 
-	const char* keyValue = cfv_get_string("nested", "object");
-	printf("For key \"object\" the value is: \"%s\"\n\r", keyValue);
+	std::cout << "Testing section separation:\n\r";
 
-	const CFV_Node* parent = cfv_get_node("nested", "object");
-	const char* nestedValue = cfv_get_string_from_node(parent, "an");
-	printf("For nested key \"an\" in object \"object\" the value is: \"%s\"\n\r", nestedValue);
+	const char* rootIsRoot = cfv_get_string(nullptr, "is_root_section");
+	const char* notRootIsRoot = cfv_get_string("first_section", "is_root_section");
+	std::cout << "The value of \"is_root_section\" in the root section is: " << rootIsRoot << "\n\r";
+	std::cout << "The value of \"is_root_section\" in other section is: " << notRootIsRoot << "\n\r";
 
-	const CFV_Node* nestedParent = cfv_get_node_from_node(parent, "an");
-	const char* deeplyNestedValue = cfv_get_string_from_node(nestedParent, "object");
-	printf("For deeply nested key \"object\" in object \"an\" in object \"object\" the value is: \"%s\"\n\r", deeplyNestedValue);
 
-	const CFV_Node* arrParent = cfv_get_node("nested", "array");
-	const char* arrValue = cfv_get_string_from_node(arrParent, "3");
-	printf("For nested key at index 0 in array \"array\" the value is: \"%s\"\n\r", arrValue);
+	std::cout << "\n\rTesting keys in quotes:\n\r";
+	const char* keyInQuotesValue = cfv_get_string("first_section", "should this keytype stay?");
+	std::cout << "The answer to \"should this keytype stay?\" is: " << keyInQuotesValue << "\n\r";
+
+	std::cout << "\n\rTesting section names with spaces:\n\r";
+	const char* spaceSectionValue = cfv_get_string("space section", "does_this_work");
+	std::cout << "The answer to \"does_this_work\" regarding spaces in section names is: " << spaceSectionValue << "\n\r";
+
+
+	std::cout << "\n\rTesting nested keys (objects and arrays):\n\r";
+	const char* objectValue = cfv_get_string("nested_keys", "an_object");
+	const CFV_Node* objectNode = cfv_get_node("nested_keys", "an_object");
+	const char* insideObjectValue1 = cfv_get_string_from_node(objectNode, "inner_key_1");
+	const char* insideObjectArrayValue = cfv_get_string_from_node(objectNode, "inner_array");
+	const CFV_Node* insideObjectArray = cfv_get_node_from_node(objectNode, "inner_array");
+	const char* doubleNestedValue = cfv_get_string_from_node(insideObjectArray, "0");
+	std::cout << "The value of the \"an_object\" object is: " << objectValue << "\n\r";
+	std::cout << "The value of the \"inner_key_1\" inside the object is: " << insideObjectValue1 << "\n\r";
+	std::cout << "The value of the \"inner_array\" inside the object is: " << insideObjectArrayValue << "\n\r";
+	std::cout << "The value of the first element in \"inner_array\" inside the object is: " << doubleNestedValue << "\n\r";
 
 	cfv_clear();
 	return 0;
